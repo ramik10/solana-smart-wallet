8d0dbc3b7286d1ae1006fe5e5fb109391b570b49
Ramik Mukherjee <ramik.14062000@gmail.com>
2024-10-08 16:24:00 +0530

    added analytics

--- solana-smart-wallet-client/src/pages/_app.tsx
+++ solana-smart-wallet-client/src/pages/_app.tsx
@@ -3,13 +3,17 @@ import type { AppProps } from "next/app";
 import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
+    <>
     <SessionProvider session={session}>
         <Component {...pageProps} />
       <ToastContainer />
     </SessionProvider>
+    <Analytics/>
+    </>
   )
 }
